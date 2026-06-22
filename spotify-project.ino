c5e482e79a60a1aad3a838fa18bc7da379649501
Denis Grigoriev <denisgrigoriev6@gmail.com>
2026-06-22 11:59:01 +0100

    Add button controls for Spotify playback
    Added switch pins for previous, pause/play, and skip functions with input pull-down configuration. Updated loop logic to handle button presses for Spotify playback control.

--- spotify-project.ino
+++ spotify-project.ino
@@ -23,9 +23,18 @@ String lastTrackname;
 Spotify sp(CLIENT_ID, CLIENT_SECRET);
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
+const int previous_switch_pin = 10; 
+const int pause_play_switch_pin = 11;
+const int skip_switch_pin = 12;
+
+
 
 void setup() {
     Serial.begin(115200);
+
+    pinMode(previous_switch_pin, INPUT_PULLDOWN);
+    pinMode(pause_play_switch_pin, INPUT_PULLDOWN);
+    pinMode(skip_switch_pin, INPUT_PULLDOWN);
 
     tft.initR(INITR_BLACKTAB); // the type of screen
     tft.setRotation(1); // this makes the screen landscape! remove this line for portrait
@@ -53,8 +62,29 @@ void setup() {
 
 void loop()
 {
+
+    int previous_switch_pin_state = digitalRead(previous_switch_pin);
+    int pause_play_switch_pin_state = digitalRead(pause_play_switch_pin);
+    int skip_switch_pin_state = digitalRead(skip_switch_pin);
+
     String currentArtist = sp.current_artist_names();
     String currentTrackname = sp.current_track_name();
+
+    if(previous_switch_pin_state = HIGH){
+        sp.skip_to_previous();
+    }
+
+    if(pause_play_switch_pin_state = HIGH){
+        if(sp.is_playing() == HIGH){
+            sp.pause_playback();
+        } else {
+            sp.start_a_users_playback();
+        }
+    }
+
+    if(skip_switch_pin_state = HIGH){
+        sp.skip_to_next();
+    }
 
     if (lastArtist != currentArtist && currentArtist != "Something went wrong" && !currentArtist.isEmpty()) {
         tft.fillScreen(ST77XX_BLACK);
